f6a64a800d4eb6465cf626ee01e0f064d90a82f8
Shreya Vinjamuri <shreyavinjamuri@fb.com>
2022-06-27 13:17:27 -0700

    setting up Parse server and connecting it to iOS app

---
+++ Instagram/Instagram/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Shreya Vinjamuri on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Instagram/Instagram/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Instagram
+//
+//  Created by Shreya Vinjamuri on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
